92d44616a7fd7d20b1081f8078eadcf6fc30291d
Le Ngoc Trung (FDN.SS2) <TrungLN@fsoft.com.vn>
2020-04-23 07:18:54 +0700

    restore src

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+  const biz_data_url = "https://raw.githubusercontent.com/trunglee155299/csv_files/master/biz.csv"
+  const sci_url = "https://raw.githubusercontent.com/trunglee155299/csv_files/master/sci.csv"
+  const eng_url = "https://raw.githubusercontent.com/trunglee155299/csv_files/master/eng.csv"
